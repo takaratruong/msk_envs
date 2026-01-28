423df74f925c59e29a4783452f0e44c66184dc1c
William Wang <wyw1@stanford.edu>
2026-01-27 16:31:26 -0800

    Update hop termination and starting pose

--- msk_envs/envs/env_hop.py
+++ msk_envs/envs/env_hop.py
@@ -22,16 +22,22 @@ class HopEnv(LanesEnv):
             cuda_graph=cuda_graph,
             target_dir=build_axis(FWD_IDX, 1.0),
         )
+        self.left_knee_qpos_idx = self.dof_id_lookup["knee_angle_l"][0]
+        self.left_knee_min_angle = -torch.deg2rad(torch.tensor(100.0, device=self.device))
         return
 
     def _get_terminated(self):
         # Get normal termination conditions
-        terminated_lanes = super()._get_terminated()
+        terminated_lanes = super()._get_terminated().bool()
 
         # Check if left toe is too low
         left_toe_pos = self.body_positions[:, self.toes_ids[0], :]
         left_toe_height = left_toe_pos[:, UP_IDX]
-        left_toe_on_ground = (left_toe_height < 0.5).float()
+        left_toe_on_ground = (left_toe_height < 0.3)
 
-        terminated = torch.max(terminated_lanes, left_toe_on_ground)
+        # Check if left knee is extended too much
+        left_knee_angle = self.joint_positions[:, self.left_knee_qpos_idx]
+        left_knee_extended = (left_knee_angle > self.left_knee_min_angle)
+
+        terminated = (terminated_lanes | left_toe_on_ground | left_knee_extended).bool()
         return terminated.detach()

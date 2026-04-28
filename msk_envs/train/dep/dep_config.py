46147e393a37166c4a5fc0eb7dcefcd5c64ed7fd
William Wang <wyw1@stanford.edu>
2026-04-28 16:02:30 -0700

    Update dep config for 30hz policy

--- msk_envs/train/dep/dep_config.py
+++ msk_envs/train/dep/dep_config.py
@@ -7,13 +7,12 @@ class DEPConfig:
     """ Whether to use DEP """
     dep_horizon: int = 8
     """ Number of steps for DEP to perform when active """
-    dep_init_steps: int = 256
     dep_p: float = 0.0037
     """ Probably of DEP being active at every step """
-    dep_buffer_size: int = 200
-    dep_bias_rate: float = 0.002
+    dep_buffer_size: int = 60
+    dep_bias_rate: float = 0.0066
     dep_kappa: float = 1000.0
-    dep_tau: int = 40
-    dep_s4avg: float = 2.0
+    dep_tau: int = 12
+    dep_s4avg: float = 1.0
     dep_regularization: float = 32
-    dep_time_dist: int = 5
+    dep_time_dist: int = 2
